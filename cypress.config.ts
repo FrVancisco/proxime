55192c681b0f2e5f7e06727e4e3b25f4169fab6c
FrVancisco <fsaccone@gmail.com>
2024-07-24 00:07:04 +0100

    amend

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      config.env.TEST_USER = process.env.TEST_USER;
-      config.env.TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD;
+      config.env.TEST_USER = process.env.TEST_USER || config.env.TEST_USER;
+      config.env.TEST_USER_PASSWORD = process.env.TEST_USER_PASSWORD || config.env.TEST_USER_PASSWORD;
       return config;
     },
     "watchForFileChanges": false
